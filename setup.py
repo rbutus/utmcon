cb4d9d01dae503ae2d9982b943929b643f33934b
rbutus <rbutustree@gmail.com>
2018-12-15 00:33:16 -0800

    Initializing package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='utmcon',
+	version='0.1',
+	description='Convert UTM to lat long.',
+	url='http://github.com/rbutus/utmcon',
+	author='Ryan Trigg',
+	author_email='rbutustree@gmail.com',
+	license='MIT',
+	packages=['utmcon'],
+	zip_safe=False,
+	)
+
